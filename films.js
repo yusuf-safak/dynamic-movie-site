6c90b44bc8f7a70345e3d37df6e0b969dac3f5c2
YUSUF ŞAFAK <92788564+yusuf-safak@users.noreply.github.com>
2022-10-18 13:51:37 +0300

    Create films.js

---
+++ films.js
@@ -0,0 +1,16 @@
+/*
+function Film(title,director,url){
+    this.title = title;
+    this.director = director;
+    this.url = url;
+}
+*/
+
+//ES6
+class Film{
+    constructor(title,director,url){
+        this.title = title;
+        this.director = director;
+        this.url = url;
+    }
+}
